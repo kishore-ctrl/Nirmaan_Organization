feefccf191cc2f44807da06b02126e4442db3073
kishore-ctrl <kishore.sret@gmail.com>
2024-11-21 12:43:56 +0530

    Add files via upload

---
+++ TaskDay2.java
@@ -0,0 +1,24 @@
+public class TaskDay2
+{
+public static void main(String args[]){
+String name1="Kishore";
+String name2="Samanthaa";
+String adjective1="happy";
+String adjective2="wonderful";
+String adjective3="excited";
+String noun1="birds";
+String noun2="chocalate";
+String noun3="guitar";
+String noun4="children";
+String noun5="ice cream";
+String noun6="robots";
+String verb1="jump";
+int number=2050;
+String place1="Hawaii";
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2+ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to" + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ".Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", ina worldwhere " + noun6 +"s ruled the world.";
+System.out.println(story);
+}
+}
+
+
