9e7a11d9cff15a584d178649e72702f6a32eee1a
kishore-ctrl <kishore.sret@gmail.com>
2024-11-22 13:40:07 +0530

    Add files via upload

---
+++ Arithmetic_opers.java
@@ -0,0 +1,49 @@
+package com.day1;
+public class Arithmetic_opers {
+	public static void main(String args[]) {
+	int a=12;
+	int b=25;
+	int d=a+b;
+	int e=a-b;
+	int f=a*b;
+	int g=a/b;
+	int h=a%b;
+	int Var=10;
+	int var2=12;
+	System.out.println("the addition of A+B=:"+d);
+	System.out.println("the subtracion of A-B=:"+e);
+	System.out.println("the multiple of A and B:" +f);
+	System.out.println("the division of A and B:"+g);
+	System.out.println("the modulus of A and B:"+h);
+	System.out.println(a>b);
+	System.out.println(a<b);
+	System.out.println(a>=b);
+	System.out.println(a<=b);
+	System.out.println(a==b);
+	System.out.println(a!=b);
+	System.out.println("logical operators"+(a>b&a<b));
+	System.out.println("logical operators"+(a<b||a>b));
+	System.out.println("Assignment optors:"+(a+=Var));
+	System.out.println("Assignment optors:"+(a-=Var));
+	System.out.println("Assignment optors:"+(a*=Var));
+	System.out.println("Assignment optors:"+(a/=Var));
+	System.out.println("Assignment optors:"+(a%=Var));
+	System.out.println("Increment oper"+(++var2));
+	System.out.println("decrement oper"+(--var2));
+	
+
+	
+
+	
+	
+	
+	
+
+	
+	
+
+	}
+}
+	
+
+
