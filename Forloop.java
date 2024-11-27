7992ce1ad48daa29fca05e4f19a818cce88a3dcb
kishore-ctrl <kishore.sret@gmail.com>
2024-11-27 13:04:01 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,15 @@
+package com.day1;
+
+public class Forloop {
+	public static void main(String args[]) {
+		for(int i=1;i<=100;i++) {
+			if(i%2==0) {
+			System.out.println("even:"+i);
+		}
+			else if(i%2!=0) {
+				System.out.println("odd:"+i);
+			}
+		}
+
+	}
+}
