8f6a870598fc574b4a767fb2aa7fe85c58e9ad54
kishore-ctrl <kishore.sret@gmail.com>
2024-11-28 11:57:31 +0530

    Add files via upload

--- Simple_Calci.java
+++ Simple_Calci.java
@@ -2,44 +2,67 @@ package com.day1;
 import java.util.Scanner;
 public class Simple_Calci {
 public static void main(String args[]) {
-	int fnum;
-	int Snum;
-	System.out.println("enter your choice");
-	System.out.println("0 for add");
-	System.out.println("1 for sub");
-	System.out.println("2 for multi");
-	System.out.println("3 for div");
-Scanner sc=new Scanner(System.in);
-int key=sc.nextInt();
-
-fnum=sc.nextInt();
-Snum=sc.nextInt();
-
-
-if(key==0) {
-int total= fnum+Snum;
-	System.out.println("addition :"+total);
-}
-else if(key==1){
-	int total=fnum-Snum;
-	System.out.println("total :"+total);
-}
-else if(key==2){
-	int total=fnum*Snum;
-	System.out.println("total :"+total);
-}
-else if(key==3) {
-	int total=fnum/Snum;
-	System.out.println("total :"+total);
-}
-else if(key==4) {
-	int total=fnum%Snum;
-	System.out.println("total :"+total);
+	Scanner sc=new Scanner(System.in);
+	boolean var= true;
+	while(var) {
+		System.out.println("press 1 for add");
+		System.out.println("press 2 for sub");
+		System.out.println("press 3 for mul");
+		System.out.println("press 4 for div");
+		System.out.println("press 5 for exit");
+		System.out.println("enter the key");
+		int key=sc.nextInt();
+		
+		
+		switch(key) {
+		case 1:{
+		System.out.print("enter the first number");
+		int fnum=sc.nextInt();
+		System.out.print("enter the second number");
+		int Snum=sc.nextInt();
+		int total=fnum+Snum;
+		System.out.println("total"+total);
+		break;
+		}
+		case 2:{
+			System.out.println("enter the first number");
+			int f2num=sc.nextInt();
+			System.out.println("enter the second number");
+			int S2num=sc.nextInt();
+			int total=f2num-S2num;
+			System.out.println("total"+total);
+			break;}
+		case 3:{
+			System.out.println("enter the first number");
+			int f3num=sc.nextInt();
+			System.out.println("enter the second number");
+			int S3num=sc.nextInt();
+			int total=f3num*S3num;
+			System.out.println("total"+total);
+			break;}
+		case 4:{
+			System.out.println("enter the first number");
+			int f4num=sc.nextInt();
+			System.out.println("enter the second number");
+			int S4num=sc.nextInt();
+			int total=f4num%S4num;
+			System.out.println("total"+total);
+			break;}
+		case 5:{
+			var=false;
+			System.out.println("program has been stopped");
+			break;
+		}
+		default:{
+			
+			System.out.println("invalid choosing");
+			break;
+		}
+			
+		}
+		
 	}
-else {
-	System.out.println("enter the correct choice");
-}
 
-}
-}
+}}
+
 
