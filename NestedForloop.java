3b074f7f6915daa7964708d923f8e38e5a0baf14
kishore-ctrl <kishore.sret@gmail.com>
2024-11-28 12:28:05 +0530

    Add files via upload

---
+++ NestedForloop.java
@@ -0,0 +1,13 @@
+package com.day1;
+
+public class NestedForloop {
+	public static void main(String args[]) {
+		for(int j=1;j<=10;j++){
+			System.out.println("iteration");
+			for(int i=1;i<5;i++) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
