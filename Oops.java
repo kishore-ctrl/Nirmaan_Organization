d0c3299c8eef94a7caf32e98b9235d6e19412776
kishore-ctrl <kishore.sret@gmail.com>
2024-11-28 13:21:16 +0530

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,24 @@
+package com.day1;
+
+public class Oops {
+		String student_name="";
+		int Roll_num=0;
+		int Age=0;
+		String Dob="";
+	 public static void main(String args[]) {
+		 Oops s1=new Oops();
+		 s1.student_name="stephen";
+		 s1.Roll_num=200320;
+		 s1.Age=22;
+		 s1.Dob="01/08/2002";
+		
+		 
+		 Oops s2=new Oops();
+		 s2.student_name="ranjith";
+		 s2.Roll_num=298372;
+		 s2.Age=22;
+		 s2.Dob="10/09/2002";
+		 System.out.println(s2.Dob);
+	 }
+
+}
