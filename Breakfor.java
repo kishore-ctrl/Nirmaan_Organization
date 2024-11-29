64f991051398023f93a8305d58a7ccce263c8c5e
kishore-ctrl <kishore.sret@gmail.com>
2024-11-29 13:16:49 +0530

    Add files via upload

---
+++ Breakfor.java
@@ -0,0 +1,25 @@
+package com.day1;//break and continue
+
+public class Breakfor {
+public static void main(String args[]) {
+//	for(int i=1;i<=200;i++){
+//		if(i>=101) {
+//			System.out.println("the program has stopped");
+//			break;
+//		}
+//		else if(i==50) {
+//			continue;
+//		}
+//		else {
+//			System.out.println(i);
+//		}
+//	}
+//}
+//}
+	String name="KISHORE";
+	String name1="stephen";
+	System.out.println(name.toLowerCase());
+	System.out.println(name1.toUpperCase());
+	
+	
+}}
---
+++ Nirmaan.java
@@ -0,0 +1,32 @@
+package com.day1;
+
+public class Nirmaan {
+	String empname="";
+	int salary=0;
+	String department="";
+	int uniqueid=0;
+	void printfn(int valu) {
+		System.out.println(valu);
+	
+	}
+
+	public static void main(String args[]) {
+		Nirmaan emp1=new Nirmaan();
+		emp1.empname="ANGEL";
+		emp1.salary=50000;
+		emp1.department="soft skills";
+		emp1.uniqueid=827666;
+		emp1.printfn(65);
+		
+		Nirmaan emp2=new Nirmaan();
+		emp2.empname="gokul";
+		emp2.salary=80000;
+		emp2.department="java developer";
+		emp2.uniqueid=828666;
+		emp2.printfn(98);
+		
+		
+	
+
+}
+	}
