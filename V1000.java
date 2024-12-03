8857b7053e2890aa36e0948da9a8024fd1b9ac88
kishore-ctrl <kishore.sret@gmail.com>
2024-12-03 12:59:36 +0530

    Add files via upload

---
+++ V1000.java
@@ -0,0 +1,16 @@
+package com.day1;
+
+public class V1000 {
+	public static void main(String args[]) {
+		String Hero="surya";
+		String str=new String("jo");
+		
+		if(Hero.equals(str)) {
+			System.out.println("Surya weds jo");
+		}
+		else {
+			System.out.println("jo hits thaseen");
+		}
+	}
+
+}
